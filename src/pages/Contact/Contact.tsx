8338cae58701f2feb9cf34d10478c289007d7916
subhram-rana-1 <subhram.rana@headout.com>
2026-03-31 13:41:48 +0530

    done

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Mail, AlertCircle, Shield, HeartHandshake } from 'lucide-react';
+import { AlertCircle, Shield, HeartHandshake } from 'lucide-react';
 import styles from './Contact.module.css';
 
 /**
